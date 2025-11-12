3aa55e9ca9c2bf4d05f502324c981e1e8f907313
Apoorv Choudhary <apoorvachoudhary506@gmail.com>
2025-11-12 16:08:53 +0530

    added vercel.json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/auth' element={<AuthPage />} />
+          <Route path='/' element={<h1>HOME PAGE</h1>} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
